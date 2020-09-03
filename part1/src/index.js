f709616dfdb9193b59895694fa209f6e06801163
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-03 12:03:11 +0300

    Beginning 1c

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,24 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Hello = (props) => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old. It is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old
       </p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app
     </div>
   )
 }
@@ -33,8 +20,6 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={26+10} />
       <Hello name={nimi} age={ika} />
-      <Hello name="Iina" age="10" />
-      <Footer />
     </>
   )
 }
