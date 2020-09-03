55ff2bc31277b4dc5d7775f82744fca7ba022d2e
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-03 12:52:47 +0300

    1c useState practice

--- part1/practice_tasks/src/index.js
+++ part1/practice_tasks/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-	const {counter} = props
+	const [ counter, setCounter ] = useState(0)
+
+	setTimeout(
+		() => setCounter(counter + 1),
+		1000
+	)
   return (
     <div>{counter}</div>
 	)
@@ -11,8 +16,8 @@ const App = (props) => {
 let counter = 1
 
 ReactDOM.render(
-    <React.StrictMode>
-      <App counter={counter} />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
