49d81475b0390d6054ddbf7f75cecc2ec99bc269
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-04 12:12:44 +0300

    List handling and conditional rendering practice completed

--- part1/practice_tasks/src/index.js
+++ part1/practice_tasks/src/index.js
@@ -1,29 +1,54 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-	const[clicks, setClicks] = useState({
-		left: 0, right: 0
-	})
-
-	const handleLeftClick = () => {
-		setClicks({ ...clicks, left: clicks.left + 1 })
-	}
-
-	const handleRightClick = () => {
-		setClicks({ ...clicks, right: clicks.right + 1 })
+const History = ({ allClicks }) => {
+	if (allClicks.length === 0) {
+		return (
+			<div>
+				the app is used by pressing the buttons
+			</div>
+		)
 	}
 
 	return (
-		<>
-			<div>
-				{clicks.left}
-				<button onClick={handleLeftClick}>left</button>
-				<button onClick={handleRightClick}>right</button>
-				{clicks.right}
-			</div>
-		</>
+		<div>
+			button press history: {allClicks.join(' ')}
+		</div>
 	)
+}
+
+const Button = ({ onClick, text }) => (
+	<button onClick={onClick}>
+		{text}
+	</button>
+)
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+	const [allClicks, setAll] = useState([])
+	
+  const handleLeftClick = () => {
+		setAll(allClicks.concat('L'))
+		setLeft(left + 1)
+	}
+
+  const handleRightClick = () => {
+		setAll(allClicks.concat('R'))
+		setRight(right + 1)
+	}
+	
+  return (
+    <>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+			</div>
+    </>
+  )
 }
 
 ReactDOM.render(
