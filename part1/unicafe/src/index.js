427f5e520d7e971a3f3c89d566dad30efc267303
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-07 13:36:29 +0300

    Part 1d task 1.9 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,16 +9,32 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const Statistics = ({ text, value }) => (
-  <p>{text} {value}</p>
-)
+const Statistics = ({ text, value }) => {
+  return <p>{text} {value}</p>
+}
+
+const GetStatistics = ({ good, neutral, bad }) => {
+  let all = good + neutral + bad
+
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <>
+      <Statistics text='good' value={good} />
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
+      <Statistics text='all' value={all} />
+      <Statistics text='average' value={(good + -1*bad)/(all)} />
+      <Statistics text='positive' value={good/all*100 + ' %'} />
+    </>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  let all = good + neutral + bad
 
   return (
     <div>
@@ -27,12 +43,7 @@ const App = () => {
       <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
       <Button text='bad' handleClick={() => setBad(bad + 1)} />
       <Header text='statistics' />
-      <Statistics text='good' value={good} />
-      <Statistics text='neutral' value={neutral} />
-      <Statistics text='bad' value={bad} />
-      <Statistics text='all' value={all} />
-      <Statistics text='average' value={(good + -1*bad)/(all)} />
-      <Statistics text='positive' value={good/all*100 + ' %'} />
+      <GetStatistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
