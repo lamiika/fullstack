f3b50bbf7261dfe7144eeb623f6c36b188c04405
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-09 13:31:31 +0300

    Part 2a task 2.1 completed

--- part2/kurssitiedot/src/index.js
+++ part2/kurssitiedot/src/index.js
@@ -36,30 +36,42 @@ const Total = (props) => {
   )
 }
 
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   )
 }
