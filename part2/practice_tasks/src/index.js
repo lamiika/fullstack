cafbebd199709ba6fa06a1e7cb1b6811c6e5baff
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-09 12:58:24 +0300

    Part 2a practice tasks ~50% complete

--- part2/practice_tasks/src/index.js
+++ part2/practice_tasks/src/index.js
@@ -1,16 +1,49 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2020-01-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2020-01-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2020-01-10T19:20:14.298Z',
+    important: true
+  }
+]
+
+const Note = ({ note }) => {
   return (
-    <>
-    </>
+    <li>{note.content}</li>
+  )
+}
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
   )
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App notes={notes} />
   </React.StrictMode>,
   document.getElementById('root')
 )
