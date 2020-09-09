efdc541f26cbc9ecec5b62ccc25530645d39329e
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-09 21:57:50 +0300

    Part 2b task 2.6 completed

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,24 +1,44 @@
 import React, { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+	const [ newName, setNewName ] = useState('')
+	
+	const addName = (event) => {
+		event.preventDefault()
+		const personObject = {
+			name: newName,
+			id: newName
+		}
+
+		setPersons(persons.concat(personObject))
+		setNewName('')
+		console.log(persons)
+	}
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+						value={newName}
+						onChange={(event) => setNewName(event.target.value)}
+					/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+			<div>
+				{persons.map(person =>
+					<Person name={person.name} key={person.id} />
+				)}
+			</div>
     </div>
   )
 
