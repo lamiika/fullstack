8191835737aa0439850e8aa0360916ea24f7844c
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-09 23:21:41 +0300

    Part 2b task 2.9* completed

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
+	const [ searchInput, setSearchInput ] = useState('')
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244', id: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 0 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
+  ])
 	const [ newName, setNewName ] = useState('')
 	const [ newNumber, setNewNumber ] = useState('')
 	
+	const filterNames = persons.filter(person => 
+		person.name.toLowerCase().includes(searchInput.toLowerCase()))
+
 	const addPerson = (event) => {
 		event.preventDefault()
 		const personObject = {
@@ -16,7 +23,7 @@ const App = () => {
 			id: newName
 		}
 
-		persons.find(person => person.name === newName) 
+		persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
 			? alert(`${newName} is already added to phonebook`)
 			: setPersons(persons.concat(personObject))
 		
@@ -28,6 +35,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+			<form>
+				<div>
+					filter shown with <input
+						value={searchInput}
+						onChange={(event) => setSearchInput(event.target.value)}
+					/>
+				</div>
+			</form>
+			<h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -47,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 			<div>
-				{persons.map(person =>
+				{filterNames.map(person =>
 					<Person name={person.name} number={person.number} key={person.id} />
 				)}
 			</div>
