636ea8af2351c4f09622966fc396d286ab6be7f5
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-14 10:45:20 +0300

    Begin part 2c

---
+++ part2/maiden_tiedot/src/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
