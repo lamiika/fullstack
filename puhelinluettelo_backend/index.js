86daf3f3310d2a685c8807bc027532d3f89a2fbc
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-25 14:15:53 +0300

    Part 3a task 3.6 completed

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -60,9 +60,21 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if (!body.name || !body.number) {
+  if (!body.name) {
     return res.status(400).json({
-      error: 'content missing'
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
     })
   }
 
