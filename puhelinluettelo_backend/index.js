a9eb1ee48d00ec30efae2da9b005dcd333c314d4
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-25 13:54:05 +0300

    Part 3a task 3.4 completed

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -47,6 +47,14 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
 })
 
 const PORT = 3001
