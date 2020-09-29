4f5a8f4e423559e6f3f32fc45ae720782c390a74
lamiika <47070356+lamiika@users.noreply.github.com>
2020-09-29 17:01:47 +0300

    Part 3c completed, task 3.18* completed

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -21,36 +21,11 @@ app.use(morgan('tiny', {
   skip: (req, res) => req.method === 'POST' && req.method === 'PUT'
 }))
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendick",
-    "number": "39-23-6423122"
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world!</h1>')
-})
-
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
-            <p>${new Date()}</p>`)
+  Person.find({}).then(persons => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`)
+  })
 })
 
 app.get('/api/persons', (req, res, next) => {
