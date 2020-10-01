b7e603490bd89a7800733d79c22011c523f2d33a
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-01 13:13:45 +0300

    Part 4b task 4.9* completed

--- part4/blogilista_backend/tests/blog_api.test.js
+++ part4/blogilista_backend/tests/blog_api.test.js
@@ -13,10 +13,18 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('all blogs are returned', async () => {
-  const result = await api.get('/api/blogs')
+describe('router', () => {
+  test('all blogs are returned', async () => {
+    const result = await api.get('/api/blogs')
 
-  expect(result.body).toHaveLength(helper.initialBlogs.length)
+    expect(result.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('the blog id field is "id", not "_id"', async () => {
+    const result = await api.get('/api/blogs')
+
+    expect(result.body[0].id).toBeDefined()
+  })
 })
 
 afterAll(() => {
