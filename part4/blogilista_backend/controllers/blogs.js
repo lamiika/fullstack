21abe708b1397ab64862517f003a264e33e11840
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-03 12:26:22 +0300

    Part 4d task 4.20* completed

--- part4/blogilista_backend/controllers/blogs.js
+++ part4/blogilista_backend/controllers/blogs.js
@@ -14,7 +14,7 @@ blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
--- part4/blogilista_backend/utils/middleware.js
+++ part4/blogilista_backend/utils/middleware.js
@@ -5,6 +5,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token missing or invalid token' })
   }
 
   logger.error(error.message)
@@ -15,9 +17,8 @@ const errorHandler = (error, request, response, next) => {
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    response.headers['Authorization'] = authorization.substring(7)
+    request.token = authorization.substring(7)
   }
-  response.header['Authorization'] = null
 
   next()
 }
