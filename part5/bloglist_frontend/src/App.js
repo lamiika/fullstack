1b5571f2fb1d45077c73a7570e57e48f3a9072bb
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-03 16:00:56 +0300

    Started part 5a tasks

---
+++ part5/bloglist_frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async () => {
+    console.log('login', username, password)
+  }
+
+  return (
+    <div>
+      {user === null ?
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username} password={password}
+          setUsername={setUsername} setPassword={setPassword}
+        /> :
+        <div>
+          <h2>blogs</h2>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist_frontend/src/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginForm = ({ handleLogin, username, password, setUsername, setPassword }) => (
+  <form onSubmit={handleLogin}>
+    login form
+  </form>
+)
+
+export default LoginForm
