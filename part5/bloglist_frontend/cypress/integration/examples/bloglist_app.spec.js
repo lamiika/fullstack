9351a87c39ac8811a7947987b88460a34b7bd08b
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-09 16:21:11 +0300

    Part 5d task 5.18 completed

--- part5/bloglist_frontend/cypress/integration/examples/bloglist_app.spec.js
+++ part5/bloglist_frontend/cypress/integration/examples/bloglist_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,5 +16,22 @@ describe('Blog app', function() {
     cy.get('[data-cy=loginSubmit]')
       .should('be.visible')
       .and('contain', 'login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('[data-cy=username]').type('mluukkai')
+      cy.get('[data-cy=password]').type('salainen')
+      cy.get('[data-cy=loginSubmit').click()
+      cy.contains('logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('[data-cy=username]').type('mluukkai')
+      cy.get('[data-cy=password]').type('wrong')
+      cy.get('[data-cy=loginSubmit').click()
+      cy.contains('wrong username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
