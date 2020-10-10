8ed49555d1f09121313c3f82081408e6f1e52f93
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-10 15:25:34 +0300

    Part 6a practice tasks until task 6.1 completed

--- part6/practice_tasks/src/index.js
+++ part6/practice_tasks/src/index.js
@@ -1,24 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import noteReducer from './reducers/noteReducer'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
 
 const App = () => {
-  return (
+  return(
     <div>
-      <div>
-        3
-      </div>
-      <div>
-        <button>plus</button>
-        <button>minus</button>
-        <button>zero</button>
-      </div>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+          )}
+      </ul>
     </div>
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const renderApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
+
+renderApp()
+store.subscribe(renderApp)
