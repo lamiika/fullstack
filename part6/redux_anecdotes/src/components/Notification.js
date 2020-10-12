4c37ebff3e30458b7a3a10dde897f0925eea75f3
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-12 15:03:05 +0300

    Part 6d task 6.19 completed, useSelect -> connect

--- part6/redux_anecdotes/src/components/Notification.js
+++ part6/redux_anecdotes/src/components/Notification.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(
-    state => state.notification.slice(-1)[0] // shows the most recent notification
-  )
-
+const Notification = (props) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
 
-  if (notification) {
+  if (props.notification) {
     return (
       <div style={style}>
-        {notification}
+        {props.notification}
       </div>
     )
   }
   return null
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification.slice(-1)[0] // shows the most recent notification
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(Notification)
