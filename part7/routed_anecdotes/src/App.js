fef53e727547bab934aa0b4f49bf4b6c61e33aff
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-13 18:08:25 +0300

    Part 7a task 7.2 completed

--- part7/routed_anecdotes/src/App.js
+++ part7/routed_anecdotes/src/App.js
@@ -1,92 +1,16 @@
 import React, { useState } from 'react'
 import {
-  Link,
   Route,
-  Switch
+  Switch,
+  useRouteMatch
 } from 'react-router-dom'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
-  return (
-    <div>
-      <Link to="/" style={padding}>anecdotes</Link>
-      <Link to="/create" style={padding}>create new</Link>
-      <Link to="/about" style={padding}>about</Link>
-    </div>
-  )
-}
-
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
-    </ul>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
-
-    See <a href='https://github.com/lamiika/fullstack/blob/master/part7/routed_anecdotes/src/App.js'>https://github.com/lamiika/fullstack/blob/master/part7/routed_anecdotes/src/App.js</a> for the source code.
-  </div>
-)
-
-const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    props.addNew({
-      content,
-      author,
-      info,
-      votes: 0
-    })
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        </div>
-        <div>
-          author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-
-}
+import Menu from './components/Menu'
+import AnecdoteList from './components/AnecdoteList'
+import Anecdote from './components/Anecdote'
+import About from './components/About'
+import Footer from './components/Footer'
+import CreateNew from './components/CreateNew'
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
@@ -127,11 +51,19 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useRouteMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find(a => Number(a.id) === Number(match.params.id))
+    : null
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
       <Switch>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdote={anecdote} />
+        </Route>
         <Route path="/about">
           <About />
         </Route>
---
+++ part7/routed_anecdotes/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovelluskehitys</a>.
+
+    See <a href='https://github.com/lamiika/fullstack/blob/master/part7/routed_anecdotes/src/App.js'>https://github.com/lamiika/fullstack/blob/master/part7/routed_anecdotes/src/App.js</a> for the source code.
+  </div>
+)
+
+export default Footer
