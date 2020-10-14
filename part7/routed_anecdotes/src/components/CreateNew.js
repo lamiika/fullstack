f93f7026152ad329adf7f2c3406edaa5d0a02c07
lamiika <47070356+lamiika@users.noreply.github.com>
2020-10-14 15:39:11 +0300

    Part 7b task 7.5 completed

--- part7/routed_anecdotes/src/components/CreateNew.js
+++ part7/routed_anecdotes/src/components/CreateNew.js
@@ -16,6 +16,13 @@ const CreateNew = (props) => {
     })
   }
 
+  const emptyFields = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -33,6 +40,7 @@ const CreateNew = (props) => {
           <input name='info' {...info} />
         </div>
         <button>create</button>
+        <button onClick={emptyFields}>reset</button>
       </form>
     </div>
   )
