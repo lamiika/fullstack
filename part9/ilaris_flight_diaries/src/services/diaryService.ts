3f4892124dd8e137110dfde25b0512ab3e6c162e
lamiika <47070356+lamiika@users.noreply.github.com>
2020-11-24 15:38:08 +0200

    Part 9c practice tasks completed

--- part9/ilaris_flight_diaries/src/services/diaryService.ts
+++ part9/ilaris_flight_diaries/src/services/diaryService.ts
@@ -1,8 +1,12 @@
 import diaryData from '../../data/diaries';
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import {
+  NewDiaryEntry,
+  NonSensitiveDiaryEntry,
+  DiaryEntry
+} from '../types';
 
-const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+const diaries: Array<DiaryEntry> = diaryData;
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
@@ -17,12 +21,24 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addEntry = () => {
-  return [];
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
-  addEntry,
-  getNonSensitiveEntries
+  getNonSensitiveEntries,
+  addDiary,
+  findById
 };
